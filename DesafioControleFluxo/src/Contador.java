8dc4fa9bd6ce99a188ae0518853f500718832341
thiago <anarxnunes@gmail.com>
2024-05-28 22:30:09 -0300

    Desafio de controle de fluxo e lançar exceção de negócio

---
+++ DesafioControleFluxo/src/Contador.java
@@ -0,0 +1,40 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Contador {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Digite o primeiro parâmetro");
+		int parametroUm = sc.nextInt();
+
+		System.out.println("Digite o segundo parâmetro");
+		int parametroDois = sc.nextInt();
+
+		try {
+			// chamando o método contendo a lógica de contagem
+			contar(parametroUm, parametroDois);
+
+		} catch (ParametrosInvalidosException exception) {
+			// imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
+			System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+		}
+		sc.close();
+	}
+
+	static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+		// validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+		if (parametroUm > parametroDois) {
+			throw new ParametrosInvalidosException();
+		}
+
+		int contagem = parametroDois - parametroUm;
+		// realizar o for para imprimir os números com base na variável contagem
+		for (int indice = 1; indice <= contagem; indice++) {
+			System.out.println("Imprimindo o número " + indice);
+		}
+		
+	}
+
+}
